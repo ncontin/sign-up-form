0f8707479f3589421de5b6a7b7be44a5d4ec4587
Nicolo Contin <95549154+ncontin@users.noreply.github.com>
2023-08-31 18:31:10 +0200

    add error when password are not matching

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const psw = document.querySelector("#password");
+const confirmPsw = document.querySelector("#confirm-password");
+const createBtn = document.getElementById("create-btn");
+const error = document.querySelector(".error");
 
+confirmPsw.addEventListener("input", () => {
+    let container = document.querySelector(".form-left");
+
+    if (psw.value !== confirmPsw.value) {
+        error.textContent = "passwords don't match";
+    } else {
+        error.textContent = "";
+    }
+});
